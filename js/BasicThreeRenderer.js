d5a6e41862b1c8fea6a82bf7477113cbd0179995
rmcgregor1990 <ram2@RAM2W7D.uk.cambridgeconsultants.com>
2014-06-13 18:24:01 +0100

    made renderer better

--- js/BasicThreeRenderer.js
+++ js/BasicThreeRenderer.js
@@ -16,7 +16,17 @@ function WebGLScene(canvas){
         params.canvas = this.CANVAS;
         params.alpha = true;
 
+        this.renderEnabled = false;
+
+        try {
         this.renderer = new THREE.WebGLRenderer(params);
+        }
+        catch(err)
+        {
+            console.error("Error Could Not Create WebGL Context");
+            throw(err);
+        }
+
         this.scene = new THREE.Scene();
         this.camera = new THREE.PerspectiveCamera(this.FOV, this.ASPECT, this.CLIP_NEAR, this.CLIP_FAR);
         this.renderer.setClearColor(colour, alpha);
@@ -33,6 +43,9 @@ function WebGLScene(canvas){
         this.controls.minDistance = 10;
         this.controls.maxDistance = 70;
 
+        this.controls.update();
+        this.controls.addEventListener( 'change', this.renderFrame.bind(this) );
+
         //this.controls.staticMoving = true;
         this.controls.dynamicDampingFactor = 0.2;
         this.controls.keys = [ 65, 83, 68 ];
@@ -45,6 +58,11 @@ function WebGLScene(canvas){
         light = new THREE.DirectionalLight( 0x002288 );
         light.position.set( -1, -1, -1 );
         this.scene.add( light );
+
+        var directionalLight = new THREE.DirectionalLight( 0xffffff, 0.3 );
+        directionalLight.position.set( 0, 1, -20 );
+        this.scene.add( directionalLight );
+
 
         light = new THREE.AmbientLight( 0x222222 );
         this.scene.add( light );
@@ -65,17 +83,20 @@ function WebGLScene(canvas){
         cube.position.x += (dimentions.x /2);
         cube.position.y += (dimentions.y /2);
         cube.position.z += (dimentions.z /2);
+        cube.separate = false;
         this.scene.add(cube);
+
     }
 
 
-    this.addSTL = function (load_path, pos, colour, name, scale_dims) {
+    this.addSTL = function (load_path, pos, rotation, colour, name, scale_dims) {
         if(typeof(scale_dims)==='undefined') scale_dims = [0,0,0];
         if(typeof(colour)==='undefined') colour = 0xffffff;
         if(typeof(name)==='undefined') name = "STLModel";
 
         var dimentions = toVec3(scale_dims);
         var position = toVec3(pos);
+        rotation = toVec3(rotation);
 
         var loader = new THREE.STLLoader();
 
@@ -92,7 +113,7 @@ function WebGLScene(canvas){
             }
 
             //rotate the geometry
-            geometry.applyMatrix(new THREE.Matrix4().makeRotationX(Math.PI /2));
+            geometry.applyMatrix(new THREE.Matrix4().makeRotationX(Math.PI /2 + rotation.x));
 
             geometry.dynamic = true;
             geometry.verticesNeedUpdate = true;
@@ -105,30 +126,87 @@ function WebGLScene(canvas){
             mesh.position.y += (dimentions.y /2);
             mesh.position.z += (dimentions.z /2);
 
+            mesh.separate = true;
+
             this.scene.add( mesh );
 
             //var helper = new THREE.BoundingBoxHelper( mesh);
             //helper.update();
             //this.scene.add(helper);
+            this.renderFrame();
 
         }.bind(this));
 
         loader.load( load_path );
     }
 
+    this.getSceneBoundingBox = function () {
+        bBox = new THREE.Box3();
 
-    this.clearScene = function() {
-        var objsToRemove = _.rest(this.scene.children, 1);
-        _.each(objsToRemove, function( object ) {
-              this.scene.remove(object);
-        }.bind(this));
+        min = new THREE.Vector3(100000,100000,100000);
+        max = new THREE.Vector3(0,0,0);
+        for (var i = this.scene.children.length - 1; i >= 0; i--) {
+            child = this.scene.children[i];
+            if ("geometry" in child && child.separate === false){
+                child.geometry.computeBoundingBox();
+                //min
+                bBox.min.x = Math.min(bBox.min.x, child.geometry.boundingBox.min.x);
+                bBox.min.y = Math.min(bBox.min.y, child.geometry.boundingBox.min.y);
+                bBox.min.z = Math.min(bBox.min.z, child.geometry.boundingBox.min.z);
+                //max
+                bBox.max.x = Math.max(bBox.max.x, child.geometry.boundingBox.max.x);
+                bBox.max.y = Math.max(bBox.max.y, child.geometry.boundingBox.max.y);
+                bBox.max.z = Math.max(bBox.max.z, child.geometry.boundingBox.max.z);
+            }
+
+        };
+        return bBox;
     }
 
-    this.render = function () {
-        requestAnimationFrame(this.render.bind(this));
+    this.updateSceneDimentions = function() {
+        var bBox = this.getSceneBoundingBox();
+        var dims = new THREE.Vector3(0,0,0);
+
+        dims.subVectors(bBox.max, bBox.min);
+
+        this.renderSizeUpdateNotify(dims);
+        return dims;
+    }
+
+    this.renderSizeUpdateNotify = function(param) {
+        if (param instanceof Function) {
+            this.renderSizeUpdateNotify.callback = param;
+        }
+
+        if (this.renderSizeUpdateNotify.callback instanceof Function) {
+            this.renderSizeUpdateNotify.callback(param);
+        }
+    }
+
+    this.clearScene = function()
+    {
+        if (typeof(this.scene) === 'undefined')
+        {
+            return;
+        }
+
+        for (var i = this.scene.children.length - 1; i >= 0; i--) {
+            child = this.scene.children[i];
+            if ("geometry" in child) {
+                this.scene.remove(child);
+            }
+        };
+    }
+
+    this.animate = function() {
+        requestAnimationFrame(this.animate.bind(this));
         this.controls.handleResize();
         this.controls.update();
+    }
+
+    this.renderFrame = function (){
         this.renderer.render(this.scene, this.camera);
+        this.updateSceneDimentions();
     }
 
     this.exportToSTL = function () {
